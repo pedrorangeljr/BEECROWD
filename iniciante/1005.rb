3cdc30a5497f1823fd0c259544674e3dc9d6aab6
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-01-09 12:10:48 -0300

    1006.rb

--- iniciante/1005.rb
+++ iniciante/1005.rb
@@ -3,6 +3,6 @@ a = Float(gets());
 print "";
 b = Float(gets());
 
-media_ponderada = ((a*3.5) + (b*7.5)) / 11;
+media_ponderada=((a*3.5)+(b*7.5))/11;
 
 printf("Media = %.5f\n", media_ponderada);
---
+++ iniciante/1006.rb
@@ -0,0 +1,10 @@
+print ""
+a = Float(gets())
+print ""
+b = Float(gets())
+print ""
+c = Float(gets())
+
+media = ((a*2) + (b*3) + (c*5))/10
+
+printf("MEDIA = %.1f\n", media)
